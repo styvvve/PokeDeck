48c6228449766663e0e6e401134a8f7d165d1a2a
styvvve <styvvvenguele@icloud.com>
2025-04-24 00:32:13 +0200

    Screen ajouterCarte + etc.

---
+++ cartePokemonCollection/View/Components/RareteEnScrollview.swift
@@ -0,0 +1,18 @@
+//
+//  RareteEnScrollview.swift
+//  cartePokemonCollection
+//
+//  Created by NGUELE Steve  on 23/04/2025.
+//
+
+import SwiftUI
+
+struct RareteEnScrollview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RareteEnScrollview()
+}
---
+++ cartePokemonCollection/View/Components/TypesGridView.swift
@@ -0,0 +1,18 @@
+//
+//  TypesGridView.swift
+//  cartePokemonCollection
+//
+//  Created by NGUELE Steve  on 23/04/2025.
+//
+
+import SwiftUI
+
+struct TypesGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypesGridView()
+}
